f5ebc31f92f75af6b7fd2fce8fb872c49199b94a
Anmol8501 <anmol.singh_cs.aiml24@gla.ac.in>
2026-03-07 22:13:36 +0530

    Create 258-AddDigits

---
+++ 258-AddDigits/258-AddDigits.java
@@ -0,0 +1,14 @@
+// Last updated: 3/7/2026, 10:13:36 PM
+class Solution {
+    public int addDigits(int num) {
+        if (num==0){
+            return 0;
+        }
+        else if (num%9==0){
+            return 9;
+        }
+        else{
+            return num%9;
+        }
+    }
+}
