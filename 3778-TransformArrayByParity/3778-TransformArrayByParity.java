4738fdc7a8d7cf2399c83d67111ac8fdca9fae0c
Anmol8501 <anmol.singh_cs.aiml24@gla.ac.in>
2026-03-07 22:10:39 +0530

    Create 3778-TransformArrayByParity

---
+++ 3778-TransformArrayByParity/3778-TransformArrayByParity.java
@@ -0,0 +1,11 @@
+// Last updated: 3/7/2026, 10:10:37 PM
+class Solution {
+    public int[] transformArray(int[] nums) {
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]%2==0) nums[i]=0;
+            else nums[i]=1;
+        }
+        Arrays.sort(nums);
+        return nums;
+    }
+}
