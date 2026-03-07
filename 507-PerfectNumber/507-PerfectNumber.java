cb6fb1745d8a242bbcba32e204eb47747d4642f9
Anmol8501 <anmol.singh_cs.aiml24@gla.ac.in>
2026-03-07 22:12:58 +0530

    Create 507-PerfectNumber

---
+++ 507-PerfectNumber/507-PerfectNumber.java
@@ -0,0 +1,6 @@
+// Last updated: 3/7/2026, 10:12:57 PM
+class Solution {
+    public boolean checkPerfectNumber(int num) {
+        return (num==6 || num==28 || num==496 || num==8128 || num==33550336);
+    }
+}
