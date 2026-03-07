a8cb2a8bb4392954f6b50982843892d372ccbb9a
Anmol8501 <anmol.singh_cs.aiml24@gla.ac.in>
2026-03-07 22:18:42 +0530

    Create 1839-DecodeXoredArray

---
+++ 1839-DecodeXoredArray/1839-DecodeXoredArray.java
@@ -0,0 +1,11 @@
+// Last updated: 3/7/2026, 10:18:42 PM
+class Solution {
+    public int[] decode(int[] encoded, int first) {
+        int[] arr=new int[encoded.length+1];
+        arr[0]=first;
+        for(int i=0;i<encoded.length;i++){
+            arr[i+1]=encoded[i]^arr[i];
+        }
+        return arr;
+    }
+}
