f4c8f692f0e93354ab3b3e9db6fe768501e4d07b
Anmol8501 <anmol.singh_cs.aiml24@gla.ac.in>
2026-03-07 22:14:00 +0530

    Create 69-Sqrtx

---
+++ 69-Sqrtx/69-Sqrtx.java
@@ -0,0 +1,16 @@
+// Last updated: 3/7/2026, 10:14:00 PM
+class Solution {
+    public int mySqrt(int x) {
+        if(x<2) return x;
+        int start =1;
+        int end=x/2;
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            long square =(long) mid*mid;
+            if(square==x)return mid;
+            else if(square<x) start=mid+1;
+            else end=mid-1;
+        }
+        return end;
+    }
+}
