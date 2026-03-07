701196297b7a0af0e8a9e9c07ab2a91492748e00
Anmol8501 <anmol.singh_cs.aiml24@gla.ac.in>
2026-03-07 22:11:15 +0530

    Create 2714-LeftAndRightSumDifferences

---
+++ 2714-LeftAndRightSumDifferences/2714-LeftAndRightSumDifferences.java
@@ -0,0 +1,21 @@
+// Last updated: 3/7/2026, 10:11:15 PM
+class Solution {
+    public int[] leftRightDifference(int[] nums) {
+        int n=nums.length;
+        int[] leftsum=new int[n];
+        int[] rightsum=new int[n];
+        int lsum=0,rsum=0;
+        for(int i=0;i<n;i++){
+            lsum+=nums[i];
+            leftsum[i]=lsum;
+            rsum+=nums[n-1-i];
+            rightsum[i]=rsum;
+        }
+        int[] arr=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=Math.abs(leftsum[i]-rightsum[n-1-i]);
+        }
+        
+        return arr;
+    }
+}
