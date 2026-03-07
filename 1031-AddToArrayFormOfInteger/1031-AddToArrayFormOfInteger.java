fe02bd8e9dd117579977788145f46c8f4f108994
Anmol8501 <anmol.singh_cs.aiml24@gla.ac.in>
2026-03-07 22:12:27 +0530

    Create 1031-AddToArrayFormOfInteger

---
+++ 1031-AddToArrayFormOfInteger/1031-AddToArrayFormOfInteger.java
@@ -0,0 +1,19 @@
+// Last updated: 3/7/2026, 10:12:25 PM
+class Solution {
+    public List<Integer> addToArrayForm(int[] num, int k) {
+        ArrayList<Integer> list = new ArrayList<>();
+        int i = num.length-1;
+
+        while (i >=0 || k > 0) {
+            if (i >= 0) {
+                k += num[i];
+                i--;
+            }
+            list.add(k % 10);
+            k /= 10;
+        }
+
+        Collections.reverse(list);
+        return list;
+    }
+}
