cb63b979faa1ba1dc729eef6a2aa05596b7cdaaf
Anmol8501 <anmol.singh_cs.aiml24@gla.ac.in>
2026-03-07 22:13:58 +0530

    Create 78-Subsets

---
+++ 78-Subsets/78-Subsets.java
@@ -0,0 +1,17 @@
+// Last updated: 3/7/2026, 10:13:57 PM
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        res.add(new ArrayList<>());
+
+        for(int i=0;i<nums.length;i++){
+            int n = res.size();
+            for(int j=0;j<n;j++){
+                List<Integer> t = new ArrayList<>(res.get(j));
+                t.add(nums[i]);
+                res.add(t);
+            }
+        }
+        return res;
+    }
+}
