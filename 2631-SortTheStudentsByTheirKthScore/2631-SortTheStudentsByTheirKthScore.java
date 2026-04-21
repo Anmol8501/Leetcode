00917ae1543b312f35112a77dd1c4241b621dd0a
Anmol Singh <anmol.singh_cs.aiml24@gla.ac.in>
2026-04-21 14:24:54 +0530

    Create 2631-SortTheStudentsByTheirKthScore

---
+++ 2631-SortTheStudentsByTheirKthScore/2631-SortTheStudentsByTheirKthScore.java
@@ -0,0 +1,7 @@
+// Last updated: 4/21/2026, 2:24:53 PM
+class Solution {
+    public int[][] sortTheStudents(int[][] score, int k) {
+        Arrays.sort(score, (a, b) -> b[k] - a[k]);
+        return score;
+    }
+}
