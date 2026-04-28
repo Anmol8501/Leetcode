b3449b5bad7486a8b3ba1b3196c696bf41ec388a
Anmol Singh <anmol.singh_cs.aiml24@gla.ac.in>
2026-04-28 20:03:35 +0530

    Create 74-SearchA2dMatrix

---
+++ 74-SearchA2dMatrix/74-SearchA2dMatrix.java
@@ -0,0 +1,12 @@
+// Last updated: 4/28/2026, 8:03:33 PM
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int row=0,col=matrix[0].length-1;
+        while(row<matrix.length && col>=0){
+            if(matrix[row][col]==target) return true;
+            else if(matrix[row][col]>target) col--;
+            else row++;
+        }
+        return false;
+    }
+}
