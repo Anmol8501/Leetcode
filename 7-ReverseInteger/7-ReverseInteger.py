e81e91ece652eced4e4a06eb2d75ef075b61224f
Anmol Singh <anmol.singh_cs.aiml24@gla.ac.in>
2026-04-28 20:04:01 +0530

    Create 7-ReverseInteger

---
+++ 7-ReverseInteger/7-ReverseInteger.py
@@ -0,0 +1,16 @@
+# Last updated: 4/28/2026, 8:03:59 PM
+class Solution:
+    def reverse(self, x: int) -> int:
+        abs_x=abs(x)
+
+        rev_x=0
+        while abs_x:
+            rev_x=10*rev_x+abs_x%10
+            abs_x//=10
+
+        if x<0: rev_x=-rev_x
+
+        if rev_x<-2**31 or rev_x>2**31-1:
+            return 0
+
+        return rev_x
