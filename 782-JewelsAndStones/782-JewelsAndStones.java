deb91f5ba84ef39f4cfbdb9029b6e1ef596517d7
Anmol Singh <anmol.singh_cs.aiml24@gla.ac.in>
2026-04-28 20:02:28 +0530

    Create 782-JewelsAndStones

---
+++ 782-JewelsAndStones/782-JewelsAndStones.java
@@ -0,0 +1,12 @@
+// Last updated: 4/28/2026, 8:02:27 PM
+class Solution {
+    public int numJewelsInStones(String jewels, String stones) {
+        int count =0;
+        for(int i=0;i<jewels.length();i++){
+            for(int j=0;j<stones.length();j++){
+                if(jewels.charAt(i)==stones.charAt(j)) count++;
+            }
+        }
+        return count;
+    }
+}
